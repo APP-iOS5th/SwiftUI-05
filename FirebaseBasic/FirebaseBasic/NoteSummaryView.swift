47c7da7f19489e14926dfeef9e1e72cdef5bcfab
jmbae <jmbae@codegrove.co.kr>
2024-04-30 11:34:36 +0900

    Add photoURL

--- FirebaseBasic/FirebaseBasic/NoteSummaryView.swift
+++ FirebaseBasic/FirebaseBasic/NoteSummaryView.swift
@@ -11,12 +11,26 @@ struct NoteSummaryView: View {
     var note: Note
     private let format: Date.FormatStyle = .dateTime.month(.wide).day().year()
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(note.title)
-                .font(.headline)
-                .fontWeight(.bold)
-            Text("\(note.date, format: format)")
-                .font(.subheadline)
+        HStack {
+            AsyncImage(url: note.photoURL) { image in
+                image
+                    .resizable()
+                    .clipShape(Circle())
+            } placeholder: {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                
+            }
+            .frame(width: 60, height: 60)
+            .padding(.trailing, 10)
+
+            VStack(alignment: .leading) {
+                Text(note.title)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                Text("\(note.date, format: format)")
+                    .font(.subheadline)
+            }
         }
     }
 }
