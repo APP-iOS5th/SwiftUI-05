e896117d099ad8e389518499001ca66762c33d18
jmbae <jmbae@codegrove.co.kr>
2024-04-30 11:14:38 +0900

    Add Author on Note

--- FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
+++ FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
@@ -25,7 +25,7 @@ struct FirebaseBasicApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            NotesListView()
+            ContentView()
         }
     }
 }
